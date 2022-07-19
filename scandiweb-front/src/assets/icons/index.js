176410868cf38b68df571f2ec436112d33e3588a
Bekjon <bekjonishpulatov8@gmail.com>
2022-07-19 23:24:31 +0500

    Add asset files

---
+++ scandiweb-front/src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import BrandSvg from "./Brand_icon.svg";
+import EmptyCart from "./Empty Cart.png";
+import dropDownSvg from "./Vector.svg";
+import cartSvg from "./Circle Icon.svg";
+
+export { BrandSvg, EmptyCart, dropDownSvg, cartSvg };
