a707886c2ddbb0de45ddad4d4699c745be73ba22
Bekjon <bekjonishpulatov8@gmail.com>
2022-10-18 21:14:46 +0500

    Fix

--- scandiweb-front/src/redux/cartSlice.js
+++ scandiweb-front/src/redux/cartSlice.js
@@ -1,17 +1,19 @@
-import { createSlice, current } from '@reduxjs/toolkit';
+import { createSlice, current } from "@reduxjs/toolkit";
 
-const localDataBase = JSON.parse(localStorage.getItem("products"))
+const localDataBase = JSON.parse(localStorage.getItem("products"));
 let localquantity = 0;
 let localTotal = 0;
 localDataBase?.forEach((product) => {
   localquantity += product.amount;
 });
 
+let localCurrency = JSON.parse(localStorage.getItem("currency")) || "USD";
+
 localDataBase?.map((product) => {
   product.prices.map((price) => {
-    if (price.currency.label === JSON.parse(localStorage.getItem("currency")).label) {
+    if (price.currency.label === localCurrency) {
       localTotal += price.amount * product.amount;
-    }else if(price.currency.label === "USD") {
+    } else if (price.currency.label === "USD") {
       localTotal += price.amount * product.amount;
     }
     return null;
@@ -26,29 +28,32 @@ const initialState = {
 };
 
 const fetchDataSlice = createSlice({
-  name: 'fetch',
+  name: "fetch",
   initialState,
   reducers: {
     addToCart(state, action) {
       if (state.productsInCart.length === 0) {
         state.productsInCart.push({ ...action.payload, amount: 1 });
-        localStorage.setItem('products', JSON.stringify(state.productsInCart));
+        localStorage.setItem("products", JSON.stringify(state.productsInCart));
 
         return;
       }
       state.productsInCart.map((product) => {
         if (
           product.id === action.payload.id &&
-          product.selectedParams['Size'] ===
-            action.payload.selectedParams['Size'] &&
-          product.selectedParams['Color'] ===
-            action.payload.selectedParams['Color'] &&
-          product.selectedParams['Capacity'] ===
-            action.payload.selectedParams['Capacity']
+          product.selectedParams["Size"] ===
+            action.payload.selectedParams["Size"] &&
+          product.selectedParams["Color"] ===
+            action.payload.selectedParams["Color"] &&
+          product.selectedParams["Capacity"] ===
+            action.payload.selectedParams["Capacity"]
         ) {
           product.amount += 1;
           console.log("bor");
-          localStorage.setItem('products', JSON.stringify(state.productsInCart));
+          localStorage.setItem(
+            "products",
+            JSON.stringify(state.productsInCart)
+          );
         }
         return null;
       });
@@ -57,16 +62,16 @@ const fetchDataSlice = createSlice({
         !state.productsInCart.some(
           (product) =>
             product.id === action.payload.id &&
-            product.selectedParams['Size'] ===
-              action.payload.selectedParams['Size'] &&
-            product.selectedParams['Color'] ===
-              action.payload.selectedParams['Color'] &&
-            product.selectedParams['Capacity'] ===
-              action.payload.selectedParams['Capacity']
+            product.selectedParams["Size"] ===
+              action.payload.selectedParams["Size"] &&
+            product.selectedParams["Color"] ===
+              action.payload.selectedParams["Color"] &&
+            product.selectedParams["Capacity"] ===
+              action.payload.selectedParams["Capacity"]
         )
       ) {
         state.productsInCart.push({ ...action.payload, amount: 1 });
-        localStorage.setItem('products', JSON.stringify(state.productsInCart));
+        localStorage.setItem("products", JSON.stringify(state.productsInCart));
       }
     },
     removeFromCart(state, action) {
@@ -79,7 +84,10 @@ const fetchDataSlice = createSlice({
             state.productsInCart.splice(index, 1);
           }
           product.amount -= 1;
-           localStorage.setItem('products', JSON.stringify(state.productsInCart));
+          localStorage.setItem(
+            "products",
+            JSON.stringify(state.productsInCart)
+          );
         }
         return null;
       });
