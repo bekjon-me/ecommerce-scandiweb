e1c923dd2aebeab65bea770bed51730a2eb0e132
Bekjon <bekjonishpulatov8@gmail.com>
2022-10-18 11:01:23 +0500

    Fix some issues

--- junior-react-endpoint/src/main.ts
+++ junior-react-endpoint/src/main.ts
@@ -3,10 +3,14 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
+const port = Number.parseInt(process.env.PORT) || 4000;
+
+
 const server = new ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers
 });
+
 
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
